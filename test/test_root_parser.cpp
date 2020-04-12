5dd4b605d5c6cb5be5993ba5f2208b87f2e6443b
Chris Wright <chris@inkyspider.co.uk>
2020-04-12 01:01:13 +0100

    Refactored valid flag tests

--- test/test_root_parser.cpp
+++ test/test_root_parser.cpp
@@ -22,26 +22,21 @@ std::function<void()> test(const std::string& arg1, bool verbose, int port) {
 TEST_CASE("single flag", "[yaap:root parser]") {
     auto cmd = yaap::Command("test");
 
-    cmd.add_flag("v|verbose|loud", parsed_args.verbose);
+    cmd.add_flag("v|verbose|l|loud", parsed_args.verbose);
 
     SECTION("no args provided") {
         cmd.run = test("", false, 0);
         cmd.execute({});
     }
 
-    SECTION("long flag 1") {
-        cmd.run = test("", true, 0);
-        cmd.execute({"--verbose"});
-    }
-
-    SECTION("long flag 2") {
-        cmd.run = test("", true, 0);
-        cmd.execute({"--loud"});
-    }
-
-    SECTION("short flag") {
-        cmd.run = test("", true, 0);
-        cmd.execute({"-v"});
+    yaap::string_vector valid_flags {"-v", "--verbose", "-l", "--loud"};
+    SECTION("valid flags") {
+        for (const auto& flag : valid_flags){
+            SECTION(flag) {
+                cmd.run = test("", true, 0);
+                cmd.execute({flag});
+            }
+        }
     }
 
     SECTION("single unknown arg") {
