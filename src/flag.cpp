efbf098cda819e28304ca681e09a1caa27f5d4f3
Chris Wright <chris@inkyspider.co.uk>
2020-04-12 21:21:29 +0100

    Added support for combined short flags

--- src/flag.cpp
+++ src/flag.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <utility>
 #include "yaap/yaap.h"
 
@@ -9,19 +10,44 @@ yaap::Flag::Flag(const std::string& id, bool& target, std::string help_message) 
     Argument(id, std::move(help_message)),
     _target(&target) {}
 
+bool string_startswith(const std::string& string, const std::string& prefix) {
+    if (string.size() < prefix.size())
+        return false;
+    for (int index = 0; index < prefix.size(); index++) {
+        if (string[index] != prefix[index])
+            return false;
+    }
+    return true;
+}
+
 yaap::string_vector yaap::Flag::process(string_vector args) {
-    for (const auto& arg : args) {
-        for (const auto& long_id : _long_ids) {
-            if (arg == "--" + long_id) {
+    for (auto& arg : args) {
+        if (!string_startswith(arg, "-"))
+            continue;
+
+        if (string_startswith(arg, "--")) {
+            for (const auto &long_id : _long_ids) {
+                if (arg == "--" + long_id) {
+                    *_target = true;
+                    args.erase(std::remove(args.begin(), args.end(), arg), args.end());
+                    return args;
+                }
+            }
+        }
+
+        for (const auto& short_id : _short_ids) {
+            if (arg == '-' + short_id) {
                 *_target = true;
                 args.erase(std::remove(args.begin(), args.end(), arg), args.end());
                 return args;
             }
-        }
-        for (const auto& short_id : _short_ids) {
-            if (arg == "-" + short_id) {
-                *_target = true;
-                args.erase(std::remove(args.begin(), args.end(), arg), args.end());
+
+            if ((arg.size() >= 2) && (arg.find(short_id) != std::string::npos)) {
+                std::string modified_arg;
+                for (auto element : arg)
+                    if (element != short_id[0])
+                        modified_arg.push_back(element);
+                arg = modified_arg;
                 return args;
             }
         }
--- test/test_root_parser.cpp
+++ test/test_root_parser.cpp
@@ -6,26 +6,28 @@
 struct Args {
     std::string arg1;
     bool verbose;
+    bool loud;
     int port;
 };
 
 Args parsed_args = {};
 
-std::function<void()> test(const std::string& arg1, bool verbose, int port) {
-    return [arg1, verbose, port]() {
+std::function<void()> test(const std::string& arg1, bool verbose, bool loud, int port) {
+    return [arg1, verbose, loud, port]() {
         REQUIRE(parsed_args.arg1 == arg1);
         REQUIRE(parsed_args.verbose == verbose);
+        REQUIRE(parsed_args.loud == loud);
         REQUIRE(parsed_args.port == port);
     };
 }
 
-TEST_CASE("single flag", "[yaap:root parser]") {
+TEST_CASE("single flag", "[flag]") {
     auto cmd = yaap::Command("test");
 
     cmd.add_flag("v|verbose|l|loud", parsed_args.verbose);
 
     SECTION("no args provided") {
-        cmd.run = test("", false, 0);
+        cmd.run = test("", false, false, 0);
         cmd.execute({});
     }
 
@@ -33,7 +35,7 @@ TEST_CASE("single flag", "[yaap:root parser]") {
     SECTION("valid flags") {
         for (const auto& flag : valid_flags){
             SECTION(flag) {
-                cmd.run = test("", true, 0);
+                cmd.run = test("", true, false, 0);
                 cmd.execute({flag});
             }
         }
@@ -45,5 +47,25 @@ TEST_CASE("single flag", "[yaap:root parser]") {
 
     SECTION("unknown arg with known flag") {
         REQUIRE_THROWS_AS(cmd.execute({"--verbose", "extra"}), yaap::UnhandledArgumentsError);
+    }
+}
+
+TEST_CASE("two flags", "[flag]") {
+    auto cmd = yaap::Command("test");
+
+    cmd.add_flag("v|verbose", parsed_args.verbose);
+    cmd.add_flag("l|loud", parsed_args.loud);
+    cmd.run = test("", true, true, 0);
+
+    SECTION("full ids") {
+        cmd.execute({"--verbose", "--loud"});
+    }
+
+    SECTION("separate short ids") {
+        cmd.execute({"-v", "-l"});
+    }
+
+    SECTION("combined short ids") {
+        cmd.execute({"-vl"});
     }
 }
